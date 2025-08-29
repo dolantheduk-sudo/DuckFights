351030ef8c90368daf91c36612478785cbb2bc65
dolantheduk-sudo <dolantheduk@gmail.com>
2025-08-29 15:58:15 -0400

    Update FightSim.cs

--- src-stubs/Sim/FightSim.cs
+++ src-stubs/Sim/FightSim.cs
@@ -1,1 +1,19 @@
+using System;
 
+namespace Drakefighting.Sim
+{
+    public class FightSim
+    {
+        public int Seed { get; }
+        private readonly Random _rng;
+
+        public FightSim(int seed)
+        {
+            Seed = seed;
+            _rng = new Random(seed);
+        }
+
+        // TODO: Inject teams, produce a log of rounds that the renderer can play back.
+        // public SimResult Run(MatchSpec spec) { ... }
+    }
+}
