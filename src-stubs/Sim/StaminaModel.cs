7ad43613f25c19155ccd06d263caee1b8dfab792
dolantheduk-sudo <dolantheduk@gmail.com>
2025-08-29 15:58:00 -0400

    Update StaminaModel.cs

--- src-stubs/Sim/StaminaModel.cs
+++ src-stubs/Sim/StaminaModel.cs
@@ -1,1 +1,11 @@
+namespace Drakefighting.Sim
+{
+    public static class StaminaModel
+    {
+        public static float DecayPerRound(int baseDrain, int styleMultiplierPercent)
+            => baseDrain * (1f + styleMultiplierPercent / 100f);
 
+        public static float LowStaminaPenalty(float staPercent)
+            => staPercent < 0.35f ? 0.6f : 1f; // damage multiplier
+    }
+}
