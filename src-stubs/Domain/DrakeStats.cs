4b37e5b9331a9d7c00a798932a99240f3d2a83c1
dolantheduk-sudo <dolantheduk@gmail.com>
2025-08-29 15:57:08 -0400

    Update DrakeStats.cs

--- src-stubs/Domain/DrakeStats.cs
+++ src-stubs/Domain/DrakeStats.cs
@@ -1,1 +1,12 @@
-
+namespace Drakefighting.Domain
+{
+    public struct DrakeStats
+    {
+        public int SPD; // Speed
+        public int STA; // Stamina
+        public int POW; // Power
+        public int SMT; // Smarts
+        public int Mood; // -100..+100 (aggression)
+        public int Luck; // 0..100
+    }
+}
