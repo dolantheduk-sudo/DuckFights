55ee51beef876f934d4609a996045ef08bda46a4
dolantheduk-sudo <dolantheduk@gmail.com>
2025-08-29 15:57:40 -0400

    Update TeamLoudout.cs

--- src-stubs/Domain/TeamLoudout.cs
+++ src-stubs/Domain/TeamLoudout.cs
@@ -1,1 +1,10 @@
+using System.Collections.Generic;
 
+namespace Drakefighting.Domain
+{
+    public class TeamLoadout
+    {
+        public List<string> DrakeIds = new(); // roster by ID
+        public MatchMode Mode;
+    }
+}
